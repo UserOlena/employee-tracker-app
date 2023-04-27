edc6d7001234b8eb228b719031b5c429813652be
Olena Pashchenko <helen.watermore@gmail.com>
2023-04-26 23:05:37 -0700

    Create connection.js file and importing it to app.js.

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+}).promise();
+
+module.exports = connection;
