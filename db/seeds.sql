e1da5102efc3c9a4488a133a641b82074eb77561
Olena Pashchenko <helen.watermore@gmail.com>
2023-04-29 20:12:38 -0700

    Include extra information into the seeds.js.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
-INSERT INTO department(name)
-VALUES  ('director'),
-        ('success manager'),
-        ('instructor'),
-        ('teacher assistant'),
-        ('tutor'),
-        ('admission');
+INSERT INTO department(id, name)
+VALUES  (1, 'director'),
+        (2, 'success manager'),
+        (3, 'instructor'),
+        (4, 'teacher assistant'),
+        (5, 'tutor'),
+        (6, 'admission');
 
 
-INSERT INTO role(title, salary)
-VALUES  ('director', 250000),
-        ('success manager', 120000),
-        ('instructor', 120000),
-        ('teacher assistant', 70000),
-        ('junior teacher assistant', 50000),
-        ('tutor', 60000),
-        ('admission manager', 110000),
-        ('admission', 60000);
+INSERT INTO role(id, title, salary, department_id)
+VALUES  (1, 'director', 250000, 1),
+        (2, 'success manager', 120000, 2),
+        (3, 'instructor', 120000, 3),
+        (4, 'teacher assistant', 70000, 4),
+        (5, 'junior teacher assistant', 50000, 4),
+        (6, 'tutor', 60000, 5),
+        (7, 'admission manager', 110000, 6),
+        (8, 'admission', 60000, 6);
 
 
-INSERT INTO employee(first_name, last_name)
-VALUES  ('Bossy', 'Man'),
-        ('Krishana', 'Anderson'),
-        ('Emamnuel', 'Jucaban'),
-        ('John', 'Doe'),
-        ('Jane', 'Doe'),
-        ('Rapunzel', 'Casing'),
-        ('Ramon', 'Sanches'),
-        ('Ryan', 'McGale');
+INSERT INTO employee(id, first_name, last_name, role_id)
+VALUES  (1, 'Bossy', 'Man', 1),
+        (2, 'Krishana', 'Anderson', 2),
+        (3, 'Emamnuel', 'Jucaban', 3),
+        (4, 'John', 'Doe', 4),
+        (5, 'Jane', 'Doe', 5),
+        (6, 'Rapunzel', 'Casing', 7),
+        (7, 'Ramon', 'Sanches', 4),
+        (8, 'Ryan', 'McGale', 8);
