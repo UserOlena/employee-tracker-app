bc09bfff4c1107a37d98ee591d9f17429856f918
Olena Pashchenko <helen.watermore@gmail.com>
2023-04-29 18:38:19 -0700

    Refactor queries.js to avoid dublicated code.

--- queries.js
+++ queries.js
@@ -5,84 +5,23 @@ const connection = require('./config/connection.js');
 async function sqlSelect(sql) {
     try {
         const [ result ] = await connection.query(sql);
-        console.table(result);
+        return result;
     } catch (error) {
         console.log(error);
     }
 }
 
 
-async function addNewDepartment(newDepartment) {
-    const sql = `
-        INSERT INTO department(name)
-        VALUES (?);`
+// if the user selects to add specific information from the prompts, the function is designed to insert this new information into the database.
+async function sqlInsert(sql, newValues, logInfo) {  
     try {
-    await connection.query(sql, [newDepartment]);
-    console.log(`${newDepartment} department has succesfully been added to the Department database table!`);
+    await connection.query(sql, newValues);
+    console.log(logInfo);
     } catch (error) {
         console.log(error);
     }
 }
-
-
-// function adds new job role, its salary and associated department_id to the role database.
-async function addRole(roleTitle, roleSalary, roleDepartmentId) {
-    const sql = `
-        INSERT INTO role(title, salary, department_id)
-        VALUES (?, ?, ?);`;
-    try {
-        await connection.query(sql, [roleTitle, roleSalary, roleDepartmentId]);
-        console.log(`New ${roleTitle} role and it's salary of ${roleSalary} has succesfully been added to the Role database table!`);
-    } catch (error) {
-        console.log(error);
-    }  
-}
-
-// function adds new employee to the employee database.
-async function addEmployee(employeeFirstName, employeeLastName, employeeRoleId, employeeManagerId) {
-    const sql = `
-        INSERT INTO employee(first_name, last_name, role_id, manager_id)
-        VALUES (?, ?, ?, ?);`;
-    try {
-        connection.query(sql, [employeeFirstName, employeeLastName, employeeRoleId, employeeManagerId]);
-        console.log(`New employee ${employeeFirstName} ${employeeLastName} has been succesfully added to the Employee database table!`);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-async function updateEmployeeRole(newRoleId, employeeId) {
-    console.log(newRoleId, employeeId)
-    const sql = `
-        UPDATE employee
-        SET role_id = ?
-        WHERE id = ?;`;
-    const values = [parseInt(employeeId), parseInt(newRoleId)];
-    try {
-        await connection.query(sql, values);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-// function that retrieves a list of current employees (id, first_name, last_name) from the employee table and their corresponding departments from the department table.
-async function employeesListForPrompt() {
-    const sql = `
-        SELECT e.id, e.first_name, e.last_name, coalesce(d.name, 'no department') as department_name 
-        FROM employee e 
-            LEFT JOIN role r on e.role_id = r.id 
-            LEFT JOIN department d on r.department_id = d.id;`;
-    try {
-        const [ rows ] = await connection.query(sql);   
-        return rows;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 
 //connection.end();
 
-module.exports = { sqlSelect, addNewDepartment, addRole, addEmployee, updateEmployeeRole, employeesListForPrompt }
+module.exports = { sqlSelect, sqlInsert }
